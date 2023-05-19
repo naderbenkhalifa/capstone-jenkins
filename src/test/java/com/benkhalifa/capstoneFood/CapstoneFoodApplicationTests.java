3a0c9f16dc8c6d670e76f7f51b6df09972ebff88
naderbenkhalifa <nader.benkhalifa78@gmail.com>
2023-05-19 08:13:03 +0100

    second version

---
+++ src/test/java/com/benkhalifa/capstoneFood/CapstoneFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benkhalifa.capstoneFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
